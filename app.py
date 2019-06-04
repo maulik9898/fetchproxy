2b04b4a31010846ea7b1dfec70f988081f4b7384
Maulik Patel <maulikp163@gmail.com>
2019-06-04 21:49:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import datetime
+import os
+import time
+
+import schedule
+
+from proxy import Proxy
+
+
+def fetch_and_check():
+    print('Fetching and checking Proxy')
+    connection = os.environ['conn']
+    print('starting time', datetime.datetime.now())
+    api = Proxy()
+    api.get_proxy()
+    print('Checking proxy...')
+    api.check(CONNECTIONS=int(connection))
+    print('ending time', datetime.datetime.now())
+    print('Done fetching and checking Proxy')
+    return {"code": 200}
+
+
+def fetch():
+    print('Fetching Proxy...')
+    connection = os.environ['conn']
+    print('starting time', datetime.datetime.now())
+    api = Proxy()
+    api.get_proxy()
+    print('ending time', datetime.datetime.now())
+    print('Done fetching Proxy')
+    return {"code": 200}
+
+
+def check():
+    print('Checking Proxy...')
+    connection = os.environ['conn']
+    print('starting time', datetime.datetime.now())
+    api = Proxy()
+    api.get_proxy()
+    print('ending time', datetime.datetime.now())
+    print('Done checking Proxy')
+    return {"code": 200}
+
+if __name__ == '__main__':
+
+    fetch()
